a4fe475fa3ea2df8b7050feaaec42712245ee243
rizperdana <perdana.rizki16@gmail.com>
2020-09-26 20:17:38 +0700

    Updates

---
+++ precache-manifest.41bdab1f3517e32f7cdb772d0a58f39d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8226b16f8fe27b81812fcdfbbc40216c",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "8ce0e12dc023a0e37d5a",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "177b678107a2352c1ac4",
+    "url": "/me/static/js/2.79695d2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.79695d2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ce0e12dc023a0e37d5a",
+    "url": "/me/static/js/main.f857a480.chunk.js"
+  },
+  {
+    "revision": "245baabdfbd726c85417",
+    "url": "/me/static/js/runtime-main.6625ee31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/me/static/media/logo.5d5d9eef.svg"
+  }
+]);
